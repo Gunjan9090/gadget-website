a6cad815fdcd9062c7a9b63872b5dc56e111c06f
Gunjan9090 <gg2354875@gmail.com>
2020-11-05 13:04:56 +0530

    commit first

--- js/index.js
+++ js/index.js
@@ -10,10 +10,18 @@
 
 
 	// });
-	var preloader=document.getElementById('loading');
+	
+  
+
+	
+
+	
+ <script type="text/javascript">
+  
+  var preloader=document.getElementById('loading');
 
   function myFunction(){
     preloader.style.display= 'none';
   }
-
-	
+</script>
+\
